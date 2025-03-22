84616e6ee9f528697dea356691b3b7dc787e4c10
VLADISLAV SHELOPUGIN <vshelopugin@pyrus.com>
2025-03-22 14:56:39 +0300

    delete unused from sample plugin

--- .storybook/fenControlAddon.js
+++ .storybook/fenControlAddon.js
@@ -1,12 +1,7 @@
-function previewAnnotations(entry = []) {
-  return [...entry, require.resolve("../FenControlAddon/dist/preview.js")];
-}
-
 function managerEntries(entry = []) {
   return [...entry, require.resolve("../FenControlAddon/dist/manager.js")];
 }
 
 module.exports = {
   managerEntries,
-  previewAnnotations,
 };
